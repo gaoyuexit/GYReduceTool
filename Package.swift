0090276cf6e65b55bb73eec05c35f1d20395e919
gaoyu <gaoyuexit@163.com>
2017-05-23 02:09:33 +0800

    complete base setting: add CommandLine and Rainbow

--- Package.swift
+++ Package.swift
@@ -3,5 +3,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "GYReduceTool"
+    name: "GYReduceTool",
+    dependencies: [
+        // 用于获取用户输入的参数
+        .Package(url: "https://github.com/jatoben/CommandLine","3.0.0-pre1"),
+        // 用户参数高亮显示  majorVersion: 2 表示 所有的2点几的版本都是可以接受的
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,70 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow
+
+
+let cli = CommandLineKit.CommandLine()
+
+/// shortFlag: "p":  表示 -p     longFlag: "project":  表示 --project   required: false  是否为必须的参数
+let projectOption = StringOption(shortFlag: "p",
+                                 longFlag: "project",
+                                 helpMessage: "Path to the project.")
+
+// MultiStringOption 字符串数组
+let resourceExtensionsOption = MultiStringOption(shortFlag: "r",
+                                          longFlag: "resource-extensions",
+                                          helpMessage: "Extensions to search.")
+
+let fileExtensionsOption = MultiStringOption(shortFlag: "f",
+                                                 longFlag: "file-extensions",
+                                                 helpMessage: "File Extensions to search.")
+
+
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+
+cli.addOptions(projectOption, resourceExtensionsOption, fileExtensionsOption, help)
+
+
+// Rainbow:  用户参数高亮显示
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    return cli.defaultFormat(s: str, type: type)
+}
+
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+// 如果有用户输入了help, 打印所有的参数
+if help.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+
+
+
+
+
+
+
+
+
+
+
